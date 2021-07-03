07343ec45d059c5aedafb818703ae5cd9bed9f98
abdulmajid <abdulmajid08180881@gmail.com>
2021-07-03 21:12:26 +0700

    added state management

--- app/src/pages/Dashboard.jsx
+++ app/src/pages/Dashboard.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+// import useFetch from '../hooks/useFetch'
+import { useSelector, useDispatch } from 'react-redux'
+import { fetchDataPersonel, setLoading } from '../store/actions/action'
 
 function Dashboard() {
+    const dispatch = useDispatch()
+    // https://randomuser.me/api/?results=4
+    // const [dataPersonel, loading, error] = useFetch('https://randomuser.me/api/?results=4')
+    // console.log(dataPersonel, 'HASIL DATA NYA');
+    const dataPersonelEmployee = useSelector(state => state.dataPersonel) 
+    console.log(dataPersonelEmployee, 'HASIL DATA');
+
+    useEffect(() => {
+        dispatch(setLoading(true))
+        dispatch(fetchDataPersonel())
+    }, [])
+
+
     return (
         <div>
-            <h1>hello world</h1>
+            <p>{JSON.stringify(dataPersonelEmployee, null, 2)}</p>        
         </div>
     )
 }
---
+++ app/src/store/actions/action.js
@@ -0,0 +1,35 @@
+import {
+    SET_DATA_PERSONEL,
+    SET_LOADING,
+    SET_ERROR
+} from '../actions/actionType'
+
+export function dataPersonel(payload) {
+    return { type: SET_DATA_PERSONEL, payload: payload }
+}
+export function setLoading(payload) {
+    return { type: SET_LOADING, payload: payload }
+}
+export function setError(payload) {
+    return { type: SET_ERROR, payload: payload }
+}
+
+// API = https://randomuser.me/api/?results=4
+
+export function fetchDataPersonel() {
+    return (dispatch) => {
+        fetch(`https://randomuser.me/api/?results=4`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data, 'INI HASIL DATA DI ACTION');
+            dispatch(dataPersonel(data.results))
+        })
+        .catch((err) => {
+            console.log(err);
+            dispatch(setError(err))
+        })
+        .finally((_) => {
+            dispatch(setLoading(false))
+        })
+    }
+}
